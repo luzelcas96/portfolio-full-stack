74031b2aac72c652e0b3fdbc7c43edad4bf37c31
Lucho Zelada <luisdanger96@gmail.com>
2022-06-26 13:24:17 -0500

    ADD: styles, colors, icons

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  yellow:{
+    300: "#FCDA69",
+  },
+
+  purple: {
+    300: "#8873EF"
+  },
+
+  orange: {
+    300: "#F98B5B"
+  },
+
+  background: "#E5E5E5",
+  white: "#FFFFFF",
+};
